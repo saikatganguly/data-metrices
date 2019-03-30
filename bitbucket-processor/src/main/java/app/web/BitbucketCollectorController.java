ed3a8f19530f2c07cb0860a2b64616654bc01c0d
Ashish Sanodia <ashishsanodia@gmail.com>
2019-03-31 00:14:57 +0530

    Adds temp controller to fetch all repos information for a project.

---
+++ bitbucket-processor/src/main/java/app/web/BitbucketCollectorController.java
@@ -0,0 +1,29 @@
+package app.web;
+
+import app.converter.BitbucketCollector;
+import app.model.BitbucketRepo;
+import app.model.CommitInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.MalformedURLException;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class BitbucketCollectorController {
+
+    private BitbucketCollector bitbucketCollector;
+
+    @Autowired
+    public BitbucketCollectorController(BitbucketCollector bitbucketCollector) {
+        this.bitbucketCollector = bitbucketCollector;
+    }
+
+    @RequestMapping("/collect")
+    public List<Map<BitbucketRepo, Map<String, CommitInfo>>> collect() throws MalformedURLException {
+        return bitbucketCollector.collect();
+    }
+
+}

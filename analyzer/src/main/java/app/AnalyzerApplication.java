436dea139b923670390e459d399aec63c9bc20b2
RandeepJagtap <jagtaprandeep001@gmail.com>
2019-03-30 07:36:59 +0100

    @rjagtap: Initial commit for analyzer app

---
+++ analyzer/src/main/java/app/AnalyzerApplication.java
@@ -0,0 +1,15 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AnalyzerApplication.class, args);
+
+    }
+
+}

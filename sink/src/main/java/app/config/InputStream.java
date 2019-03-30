59eec5375e7092ef887b7c71fa7b47f319c77661
Ashish Sanodia <ashishsanodia@gmail.com>
2019-03-30 14:37:54 +0530

    Changes sink to (directly read from jenkins-raw-data topic)--->(convert)--->(save in mobngo-db)

--- sink/src/main/java/app/config/InputStream.java
+++ sink/src/main/java/app/config/InputStream.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.messaging.SubscribableChannel;
 
 public interface InputStream {
-    String INPUT = "message-in";
+    String INPUT = "jenkins-raw-data";
 
     @Input(INPUT)
     SubscribableChannel inboundChannel();
--- sink/src/main/java/app/receiver/JenkinsMesageReceiver.java
+++ sink/src/main/java/app/receiver/JenkinsMesageReceiver.java
@@ -1,27 +1,29 @@
 package app.receiver;
 
 import app.config.InputStream;
-import app.model.BuildDetailsModel;
-import org.bson.Document;
+import app.converter.JsonToObjectConvertor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Map;
-
 @Component
 public class JenkinsMesageReceiver {
-
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    @StreamListener(InputStream.INPUT)
-    public void handleMessages(@Payload Map<String, List<BuildDetailsModel>> buildInfo) {
-        System.out.println("Build info : "+buildInfo);
+    private ObjectMapper mapper = new ObjectMapper();
 
-        mongoTemplate.insert(buildInfo , "test1");
+    @StreamListener(InputStream.INPUT)
+    public void handleMessages(@Payload String completeJenkinsBuildInfo) {
+        String processedJenkinsBuildInfo = "";
+        try {
+            processedJenkinsBuildInfo = mapper.writeValueAsString(JsonToObjectConvertor.buildJenkinsDetails(completeJenkinsBuildInfo));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        mongoTemplate.insert(processedJenkinsBuildInfo, "test1");
     }
 }

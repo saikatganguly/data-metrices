a077e525103cd859b067d6cff5cedc1d28d10875
Ashish Sanodia <ashishsanodia@gmail.com>
2019-04-01 23:08:41 +0530

    Adds SonarDataReceiver to persist sonar processed data in mongo

--- sink/src/main/java/app/config/Channels.java
+++ sink/src/main/java/app/config/Channels.java
@@ -6,11 +6,15 @@ import org.springframework.messaging.SubscribableChannel;
 public interface Channels {
     String JENKINS_PROCESSED_DATA = "jenkins-processed-data";
     String BITBUCKET_PROCESSED_DATA = "bitbucket-processed-data";
+    String SONAR_PROCESSED_DATA = "sonar-processed-data";
 
     @Input(JENKINS_PROCESSED_DATA)
     SubscribableChannel jenkinsInboundChannel();
 
     @Input(BITBUCKET_PROCESSED_DATA)
     SubscribableChannel bitbucketInboundChannel();
+
+    @Input(SONAR_PROCESSED_DATA)
+    SubscribableChannel sonarInboundChannel();
 
 }
---
+++ sink/src/main/java/app/receiver/SonarDataReceiver.java
@@ -0,0 +1,24 @@
+package app.receiver;
+
+import app.config.Channels;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class SonarDataReceiver {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @StreamListener(Channels.SONAR_PROCESSED_DATA)
+    public void handleMessages(@Payload List<Map<String, String>> sonarProjectInfo) {
+        System.out.println("sonarProjectInfo = " + sonarProjectInfo);
+
+        mongoTemplate.insert(sonarProjectInfo, "sonar-processed-data");
+    }
+}

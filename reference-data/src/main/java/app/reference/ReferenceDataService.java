6208bba0fae69577d9061b3c9b84091330a3733c
RandeepJagtap <jagtaprandeep001@gmail.com>
2019-04-03 16:43:27 +0100

    Bug Fixes in Analyzer and sample data creation

--- reference-data/src/main/java/app/reference/ReferenceDataService.java
+++ reference-data/src/main/java/app/reference/ReferenceDataService.java
@@ -7,9 +7,9 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static java.util.Collections.emptyList;
 import static java.util.Objects.isNull;
@@ -24,32 +24,57 @@ public class ReferenceDataService {
 
     @PostConstruct
     public void init() {
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
         ObjectMapper mapper = new ObjectMapper();
         try {
             userProvidedData = mapper.readValue(userProvidedDataFile.getFile(), Organization.class);
+            //createData();
         } catch (IOException e) {
             System.out.println("Exception occured: " + e);
         }
 
-        System.out.println("Organization Data: " + userProvidedData);
+        //System.out.println("Organization Data: " + userProvidedData);
+        System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
     }
 
-    public List<Repo> getReposByProject(String project) {
+    public List<Repo> getReposByProject(String transactionCycle, String geography, String project) {
         if (isNull(project) || project.isEmpty()) {
             return emptyList();
         }
-        List<Repo> repos = new ArrayList<>();
-        for (TransactionCycle transactionCycle : userProvidedData.getTransactionCycles()) {
-            for (Geography geography : transactionCycle.getGeographies()) {
-                for (Project proj : geography.getProjects()) {
-                    if (proj.getProjectName().equalsIgnoreCase(project)) {
-                        repos = proj.getRepos();
+        for (TransactionCycle tc : userProvidedData.getTransactionCycles()) {
+            if (tc.getTransactionCycleName().equalsIgnoreCase(transactionCycle)) {
+                for (Geography geography1 : tc.getGeographies()) {
+                    if (geography1.getGeographyName().equalsIgnoreCase(geography)) {
+                        for (Project proj : geography1.getProjects()) {
+                            if (proj.getProjectName().equalsIgnoreCase(project)) {
+                                return proj.getRepos();
+                            }
+                        }
                     }
                 }
             }
         }
 
-        return repos;
+        return emptyList();
+    }
+
+
+    public List<Repo> getReposByProject(String project) {
+        if (isNull(project) || project.isEmpty()) {
+            return emptyList();
+        }
+        for (TransactionCycle tc : userProvidedData.getTransactionCycles()) {
+            for (Geography geography1 : tc.getGeographies()) {
+                for (Project proj : geography1.getProjects()) {
+                    if (proj.getProjectName().equalsIgnoreCase(project)) {
+                        return proj.getRepos();
+                    }
+                }
+            }
+        }
+
+        return emptyList();
     }
 
     public Repo getRepo(String repoName) {
@@ -72,44 +97,117 @@ public class ReferenceDataService {
         return null;
     }
 
-    public List<Project> getProjectsByGeography(String geography) {
+    public List<Project> getProjectsByGeography(String transactionCycle, String geography) {
         if (isNull(geography) || geography.isEmpty()) {
             return emptyList();
         }
-        List<Project> projects = new ArrayList<>();
-        for (TransactionCycle transactionCycle : userProvidedData.getTransactionCycles()) {
-            for (Geography geo : transactionCycle.getGeographies()) {
-                if (geo.getGeographyName().equalsIgnoreCase(geography)) {
-                    projects = geo.getProjects();
+        for (TransactionCycle tc : userProvidedData.getTransactionCycles()) {
+            if (tc.getTransactionCycleName().equalsIgnoreCase(transactionCycle)) {
+                for (Geography geo : tc.getGeographies()) {
+                    if (geo.getGeographyName().equalsIgnoreCase(geography)) {
+                        return geo.getProjects();
+                    }
                 }
             }
         }
-        return projects;
+        return emptyList();
     }
 
     public List<Geography> getGeographyByTransactionCycle(String transactionCycle) {
         if (isNull(transactionCycle) || transactionCycle.isEmpty()) {
             return emptyList();
         }
-        List<Geography> geographies = new ArrayList<>();
         for (TransactionCycle tc : userProvidedData.getTransactionCycles()) {
             if (tc.getTransactionCycleName().equalsIgnoreCase(transactionCycle)) {
-                geographies = tc.getGeographies();
+                return tc.getGeographies();
             }
         }
-        return geographies;
+        return emptyList();
     }
 
     public List<TransactionCycle> getTransactionCyclesByOrganization(String organization) {
         if (isNull(organization) || organization.isEmpty()) {
             return emptyList();
         }
-        List<TransactionCycle> organizations = new ArrayList<>();
         if (userProvidedData.getOrganizationName().equalsIgnoreCase(organization)) {
-            organizations = userProvidedData.getTransactionCycles();
+            return userProvidedData.getTransactionCycles();
         }
-        return organizations;
+        return emptyList();
     }
+
+
+    public void createData() throws IOException {
+        int count = 101;
+        List<BuildDetailsModel> buildDetailsModels = new ArrayList<>();
+
+        for (Repo repo : getRepos()) {
+            for (int i = 0; i <= getRandomNumberInRange(10, 15); i++) {
+                BuildDetailsModel model = getBuildDetailsModel(repo, Integer.toString(count));
+                buildDetailsModels.add(model);
+                count++;
+            }
+        }
+
+        DataCreationRequest request = new DataCreationRequest();
+        request.setBuildDetailsModels(buildDetailsModels);
+
+        System.out.println("+++++++++++++++++++Generated Data+++++++++++++++++++++++++");
+        System.out.println(request);
+        System.out.println("+++++++++++++++++++Done Data+++++++++++++++++++++++++");
+
+        ObjectMapper om = new ObjectMapper();
+        om.writeValue(new File("raje-is-chu.json"), request);
+
+    }
+
+    private List<Repo> getRepos() {
+        List<Repo> repos = new ArrayList<>();
+
+        for (TransactionCycle transactionCycle : userProvidedData.getTransactionCycles()) {
+            for (Geography geography : transactionCycle.getGeographies()) {
+                for (Project proj : geography.getProjects()) {
+                    for (Repo repo : proj.getRepos()) {
+                        repos.add(repo);
+                    }
+                }
+            }
+        }
+
+        return repos;
+    }
+
+    private BuildDetailsModel getBuildDetailsModel(Repo repo, String i) {
+        byte[] array = new byte[7]; // length is bounded by 7
+
+        new Random().nextBytes(array);
+        String commit = UUID.randomUUID().toString();
+
+        new Random().nextBytes(array);
+        String displayName = UUID.randomUUID().toString();
+
+        GitDetails gitDetails = new GitDetails(repo.getUrl(), "master", commit);
+        CauseDetails causeDetails = new CauseDetails("some-cause", "some-user", "some-user-id", "some-upstream");
+        return new BuildDetailsModel(false, "job", displayName, (long) getRandomNumberInRange(500, 800),
+                (long) getRandomNumberInRange(350, 400), "executor", repo.getBuildJob(), i, true, getRandomNumberInRange(1, 100), "queue", "SUCCESS", (long) createRandomDate().getTime(), gitDetails.getRepo(), gitDetails, causeDetails);
+    }
+
+    private Date createRandomDate() {
+        int day = getRandomNumberInRange(14, 28);
+        int month = 3;
+        int year = 119;
+        return new Date(year, month, day);
+    }
+
+    private int getRandomNumberInRange(int min, int max) {
+
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+
 
     public Repo getRepoFromUrl(String repoUrl) {
         if (isNull(repoUrl) || repoUrl.isEmpty()) {
--- sink/src/main/java/app/controller/DataCreationController.java
+++ sink/src/main/java/app/controller/DataCreationController.java
@@ -1,10 +1,18 @@
 package app.controller;
 
 import app.controller.request.DataCreationRequest;
+import app.model.BuildDetailsModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
 @RestController
 @RequestMapping("/sink/data/")
@@ -12,18 +20,33 @@ public class DataCreationController {
 
     private MongoTemplate mongoTemplate;
 
+    @Value("input-file.json")
+    private ClassPathResource inputFIle;
+
+
     @Autowired
     public DataCreationController(MongoTemplate mongoTemplate) {
         this.mongoTemplate = mongoTemplate;
     }
 
-    @RequestMapping(path = "/jenkins", method = RequestMethod.POST)
+    @RequestMapping(path = "/jenkins", method = RequestMethod.POST, produces = APPLICATION_JSON_UTF8_VALUE, consumes = APPLICATION_JSON_UTF8_VALUE)
     @ResponseStatus(HttpStatus.ACCEPTED)
     @ResponseBody
-    public void createJenkinsData(@RequestBody DataCreationRequest request) {
-        mongoTemplate.insert(request.getBuildDetailsModels(), "jenkins-processed-data");
-    }
+    public void createJenkinsData() {
 
+        ObjectMapper om = new ObjectMapper();
+        try {
+            DataCreationRequest dataCreationRequest = om.readValue(inputFIle.getFile(), DataCreationRequest.class);
+
+            for (BuildDetailsModel buildDetailsModel : dataCreationRequest.getBuildDetailsModels()) {
+                mongoTemplate.insert(buildDetailsModel);
+            }
+        } catch (IOException e) {
+            System.out.println("Exception occured: " + e);
+        }
+
+    }
+/*
     @RequestMapping(path = "/sonar", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.ACCEPTED)
     @ResponseBody
@@ -36,5 +59,5 @@ public class DataCreationController {
     @ResponseBody
     public void createBitbucketData(@RequestBody DataCreationRequest request) {
         mongoTemplate.insert(request.getBitbucketRepoInfo(), "bitbucket-processed-data");
-    }
+    }*/
 }
